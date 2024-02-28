1ae0e3faf10ef61d264c25cd0f95ea6a1d4580ae
Alf <cecerealfonso98@gmail.com>
2024-02-28 16:54:39 +0100

    first login

--- Scarpe&Co/Controllers/HomeController.cs
+++ Scarpe&Co/Controllers/HomeController.cs
@@ -57,7 +57,41 @@ namespace Scarpe_Co.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Login(Utente utente)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ScarpeDB"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionString);
 
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Utenti WHERE username = @username AND password = @password";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@username", utente.Username);
+                cmd.Parameters.AddWithValue("@password", utente.Password);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    Utente user = new Utente(reader["username"].ToString(), reader["password"].ToString(), Convert.ToBoolean(reader["isAdmin"]));
+                    Session["user"] = user;
+                }
+                else
+                {
+                    Session["user"] = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally { conn.Close(); }
+
+            return RedirectToAction("Index");
+        }
         public ActionResult Details(int? id)
         {
             Prodotto product = null;
